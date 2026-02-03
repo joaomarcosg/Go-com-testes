987406db20e7e7c8a94ec14ccbbff85fb39d4572
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-03 16:01:32 -0300

    REFACTOR: refactor Hello function adding prefix features in greetingPrefix function

--- hello/hello.go
+++ hello/hello.go
@@ -8,17 +8,22 @@ const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjuor, "
 
 func Hello(name, language string) string {
+	if name == "" {
+		name = "Mundo"
+	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := portugueseHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = portugueseHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
