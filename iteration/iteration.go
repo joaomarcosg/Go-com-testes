a51f6e3aa327c9a2ef4efe25d9aa8fc4c4d1347f
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-06 15:31:38 -0300

    REFACTOR: adds number of iterations parameter

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,7 +2,7 @@ package iteration
 
 const iterationsQuantity = 5
 
-func Iteration(character string) string {
+func Iteration(character string, numberOfIteration int) string {
 	var iterations string
 	for range iterationsQuantity {
 		iterations += character
