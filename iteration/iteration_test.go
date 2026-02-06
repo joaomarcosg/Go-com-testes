0d5cb88446430ae37dc03f573dbdfeed6b2a60c4
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-06 15:41:57 -0300

    TEST: adds an example function

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIteration(t *testing.T) {
 	iterations := Iteration("a", 5)
@@ -15,4 +18,10 @@ func BenchMarkIteration(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Iteration("a", 5)
 	}
+}
+
+func ExampleIteration() {
+	iteration := Iteration("a", 5)
+	fmt.Println(iteration)
+	// Output: aaaaa
 }
