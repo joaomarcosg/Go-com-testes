70e8029d10f431bf85cdf979ad90bab7a0c7b8f7
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-10 15:55:30 -0300

    TEST: adds slices substests

--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -3,12 +3,26 @@ package arraysandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	result := Sum(numbers)
-	expected := 15
+		result := Sum(numbers)
+		expected := 15
 
-	if expected != result {
-		t.Errorf("result '%d', expected '%d', given %v", result, expected, numbers)
-	}
+		if expected != result {
+			t.Errorf("result '%d', expected '%d', given %v", result, expected, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		result := Sum(numbers)
+		expected := 6
+
+		if expected != result {
+			t.Errorf("result '%d', expected '%d', given %v", result, expected, numbers)
+		}
+	})
+
 }
